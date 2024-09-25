749055d7dd69d010b914a380f9b64e0ffd38f9cb
KTrain5369 <69028025+KTrain5169@users.noreply.github.com>
2024-09-25 19:49:31 +0700

    Neaten arg parses

--- cli.py
+++ cli.py
@@ -5,12 +5,14 @@ import argparse
 # argparsing
 def main():
     parser = argparse.ArgumentParser(description="Fibonacci counting")
-    parser.add_argument("--count", required=True, 
+    parser.add_argument("--count", required=True,
                         help="Count fibonacci until this number.")
-    parser.add_argument("--display", required=True, help="Display either only the number in the --count position, or all numbers leading up to and including that.")
+    parser.add_argument("--display", required=True,
+                        help="Defines how the output is displayed.")
     parser.add_argument("--copy", action="store_true",
                         help="Copy the result.")
-    parser.add_argument("--print", action="store_true", help="Print the result in the terminal.")
+    parser.add_argument("--print", action="store_true",
+                        help="Print the result in the terminal.")
 
     args = parser.parse_args()
 
